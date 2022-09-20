ad2e8ce5f01fb22db18a9966ab4f5fdc3ec713ed
steve4589 <65893555+steve4589@users.noreply.github.com>
2022-09-20 23:58:11 +0900

    SDL기본그리기 - 실습(수정)
    이미지가 출력이 안됬었습니다

--- include/Game.h
+++ include/Game.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <SDL2/SDL_image.h>
 #include <SDL2/SDL.h>
 
 class Game
@@ -7,12 +8,13 @@ public:
 	Game() { }
 	~Game() { }
 
-	bool init(const char* title,int ,int, int, int, int flags);
+	bool init(const char* title, int, int, int, int, int flags);
 	void render();
 	void update();
 	bool running();
 	void handleEvents();
 	void clean();
+	void ImgTexture();
 
 private:
 	SDL_Window* m_pWindow;
@@ -20,6 +22,7 @@ private:
 	bool m_bRunning;
 
 	SDL_Texture* m_pTexture;
+	SDL_Texture* ui_pTexture;
 	SDL_Rect m_sourceRectangle; //원본 사각형
 	SDL_Rect m_destinationRectangle; //대상 사각형
 };
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,22 +1,9 @@
+#pragma once
 #include "Game.h"
+#include "iostream"
 
 bool Game::init(const char* title, int xpos, int ypos, int width, int height, int flags)
 {
-	//Textture 생성
-	SDL_Surface* pTempSurface = SDL_LoadBMP("assets/rider.bmp");
-	m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
-	SDL_FreeSurface(pTempSurface);
-	//원본 상자 너비/높이 설정
-	SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
-	//SDL_QueryTexture(SDL_Texture textur, Uint32* format, int* access, int* x, int* y)
-	//format -> 해당 format으로 칠할건지 / access -> 대상 텍스쳐를 아예 칠할건지?
-
-	m_destinationRectangle.w = m_sourceRectangle.w;
-	m_destinationRectangle.h = m_sourceRectangle.h;
-
-	m_destinationRectangle.x = m_sourceRectangle.x = 0;
-	m_destinationRectangle.y = m_sourceRectangle.y = 0;
-
 	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
 		m_pWindow = SDL_CreateWindow(
 			title, xpos, ypos, width, height, flags);
@@ -24,6 +11,26 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
 			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 
 			if (m_pRenderer != 0) {
+				//Textture 생성
+				SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+
+				m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+
+				SDL_FreeSurface(pTempSurface);
+				//원본 상자 너비/높이 설정
+				SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+				//SDL_QueryTexture(SDL_Texture textur, Uint32* format, int* access, int* x, int* y)
+				//format -> 해당 format으로 칠할건지 / access -> 대상 텍스쳐를 아예 칠할건지?
+
+				m_destinationRectangle.w = m_sourceRectangle.w;
+				std::cout << m_sourceRectangle.w << "    " << m_destinationRectangle.w << "\n";
+				m_destinationRectangle.h = m_sourceRectangle.h;
+				std::cout << m_sourceRectangle.h << "    " << m_destinationRectangle.h << "\n";
+
+				m_destinationRectangle.x = m_sourceRectangle.x = 0;
+				m_destinationRectangle.y = m_sourceRectangle.y = 0;
+				std::cout << m_sourceRectangle.x << "    " << m_destinationRectangle.y;
+
 				SDL_SetRenderDrawColor(
 					m_pRenderer, 255, 255, 255, 255);
 			}
@@ -46,9 +53,8 @@ void Game::update() {}
 void Game::render()
 {
 	SDL_RenderClear(m_pRenderer);
+	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
 	SDL_RenderPresent(m_pRenderer);
-
-	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle); //텍스쳐에 설정한 사진 올리기 
 }
 bool Game::running()
 {
@@ -57,9 +63,9 @@ bool Game::running()
 void Game::handleEvents()
 {
 	SDL_Event event;
-	while (SDL_PollEvent(&event)) //작동중 이벤트를 계속 인식하기 위해서라고 생각합니다,  
-	{                             //사실 이미 main의 while내에서 작동하기 때문에 여기서 반복할 필요는 없다고 생각되지만, 
-		switch (event.type)       //그래도 추측한다면 입력에 더 빨리 반응할 수 있을것 같다고 생각합니다
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
 		{
 		case SDL_MOUSEBUTTONDOWN:
 			m_bRunning = false;
