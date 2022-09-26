a0f4bac9e64e66f14b69125d9d637507d15e0d8e
steve4589 <65893555+steve4589@users.noreply.github.com>
2022-09-26 19:30:40 +0900

    xls 커스텀 스폰1
    엑셀 파일에서 1줄씩 노트 리스폰 위치를 가져옵니다

--- src/NoteManager.cpp
+++ src/NoteManager.cpp
@@ -1,5 +1,4 @@
 #pragma once
-#include <iostream>
 #include <iostream> 
 #include <vector>
 #include <string>
@@ -7,6 +6,9 @@
 #include<fstream>
 #include <regex>
 #include "ExcelFormat.h"
+#include <SDL2/SDL_stdinc.h>
+#include <SDL2/SDL_timer.h>
+#include<sstream>
 
 using namespace ExcelFormat;
 using namespace std;
@@ -19,5 +21,39 @@ class NoteManager
 private:
 	float nextSpawnDelay;
 	float curSpawnDelay;
+
+	BasicExcel xls;
+
+	//GameManager -> static int curStage;
+
+	void ReadLineToXls()
+	{
+		BasicExcelWorksheet* sheet = nullptr;
+		sheet = xls.GetWorksheet(0); //어차피 1개만 계속 쓰기 때문에
+
+		for (int i = 1; i < sheet->GetTotalCols(); ++i)
+		{
+			string line = sheet->Cell(1, i)->GetString(); 
+			string buffer;
+			istringstream desline(line);
+
+			getline(desline, buffer, '/');
+			nextSpawnDelay = stof(buffer);
+			
+			cout << nextSpawnDelay;
+			/*
+			while (getline(desline, buffer, ' '))
+			{
+
+			}*/
+			float startTime;
+			startTime = SDL_GetTicks();
+
+			do {
+				curSpawnDelay = SDL_GetTicks() - startTime;
+				nextSpawnDelay -= curSpawnDelay;
+			} while (curSpawnDelay < nextSpawnDelay);
+		}
+	}
 
 };
