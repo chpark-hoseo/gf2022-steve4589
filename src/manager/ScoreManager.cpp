4a8a36ac6bb649eab37a951e2f1c6896a6d43618
steve4589 <65893555+steve4589@users.noreply.github.com>
2022-12-04 03:16:26 +0900

    기말 클래스 다이어그램
    https://drive.google.com/file/d/12p5KHnAiVvQagz1MLadtnEdQFkmzYtSq/view?usp=sharing

--- src/manager/ScoreManager.cpp
+++ src/manager/ScoreManager.cpp
@@ -3,7 +3,7 @@
 
 ScoreManager* ScoreManager::s_pInstance = NULL;
 
-ScoreManager::~ScoreManager() { delete playScore; }
+ScoreManager::~ScoreManager() { delete playScore; } //
 
 void ScoreManager::AddScoreSprite(SDLGameObject* getPlayScore) { playScore = getPlayScore; }
 void ScoreManager::ChangeScoreSprite() { playScore->SetSpriteFrame(grade); }
